540fe357e265a935fe736120042852ae2487c86e
makmizi <makaiharris@gmail.com>
2021-11-14 13:33:08 -0800

    fix db. Create loop through all groups on homepage

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Profile, GolfGroup
 
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(GolfGroup)
---
+++ main_app/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'location', 'img', 'bio', 'handicap', 'favorite_course']
+
+
+
+def form_validation_error(form):
+    msg = ""
+    for field in form:
+        for error in field.errors:
+            msg += "%s: %s \\n" % (field.label if hasattr(field, 'label') else 'Error', error)
+    return msg
