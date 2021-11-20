8e603809e23353b1ecd56cfbcc736530e4522cb3
makmizi <makaiharris@gmail.com>
2021-11-20 02:29:12 -0800

    no thanks

--- golfhub/settings.py
+++ golfhub/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import django_heroku
 from pathlib import Path
 import os
+import psycopg2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
@@ -146,4 +147,8 @@ LOGOUT_REDIRECT_URL = '/'
 # Configure Django App for Heroku.
 DEBUG = False
 SECURE_SSL_REDIRECT = True
+ALLOWED_HOSTS = ['*.herokuapp.com']
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
 django_heroku.settings(locals())
